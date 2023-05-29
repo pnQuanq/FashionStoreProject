8221d8907490f31c8d3e2cebce6114ff65e20e88
pnQuanq <phamquang13103@gmail.com>
2023-05-29 15:34:54 +0700

    create template, static folder

--- FashionStore/FashionStore/settings.py
+++ FashionStore/FashionStore/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path(BASE_DIR, 'templates') 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'FashionStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "public/static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- FashionStore/accounts/models.py
+++ FashionStore/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from base.models import BaseModel
 # Create your models here.
+class Profile(BaseModel):
+    user = models.OneToOneField(User , on_delete=models.CASCADE , related_name="profile")
+    is_email_vertified = models.BooleanField(default=False, null=True, blank=True)
+    profile_image = models.ImageField(upload_to='profile')
+
