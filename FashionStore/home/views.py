96c358fcae2d44f9bb7bf571d32703401e63c22d
pnQuanq <phamquang13103@gmail.com>
2023-05-31 17:23:25 +0700

    search func

--- FashionStore/home/views.py
+++ FashionStore/home/views.py
@@ -5,6 +5,11 @@ def index(request):
 
     q = request.GET.get('q')
 
-    context = {'products' : Product.objects.all()}
+    if 'que' in request.GET:
+        que = request.GET['que']
+        products = Product.objects.filter(product_name__icontains=que)
+    else:
+        products = Product.objects.all()
+    context = {'products' : products}
     return render(request , 'home/index.html' , context)
 
